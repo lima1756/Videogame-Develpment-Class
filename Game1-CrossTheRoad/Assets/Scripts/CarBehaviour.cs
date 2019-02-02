e8aa4a5c2b884dd7c9975dc7618853b31dcd438d
Iván Morett <luisivanmorett@gmail.com>
2019-02-02 14:56:50 -0600

    First game

---
+++ Game1-CrossTheRoad/Assets/Scripts/CarBehaviour.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarBehaviour : MonoBehaviour
+{
+
+    public float speed = 4f;
+    public int points = 10;
+    public float cooldown = .5f;
+    private float speedSave;
+    private float direction;
+    private int startPos;
+    private float nextDamage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speedSave = speed;
+        direction = 0;
+        startPos = (int)Mathf.Abs(Mathf.Ceil(transform.position.x))+1;
+        nextDamage = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, 0, -speed * Time.deltaTime, Space.Self);
+        if (transform.position.x > startPos || transform.position.x < -startPos)
+        {
+            transform.Rotate(0, (direction + 180) % 360, 0);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "chicken")
+        {
+            //Debug.Log(this.name + " collision with " + other.name);
+            speed = 0;
+            MoveChicken.changePoints(-points);
+            nextDamage = Time.time + cooldown;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.name == "chicken" && Time.time > nextDamage)
+        {
+            MoveChicken.changePoints(-points/5);
+            nextDamage = Time.time + cooldown;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.name == "chicken")
+        {
+            speed = speedSave;
+        }
+    }
+}
---
+++ Game1-CrossTheRoad/Assets/Scripts/MoveChicken.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoveChicken : MonoBehaviour
+{
+
+    public float speed = 10f;
+    public Text textPoints;
+    private static int myPoints;
+    private Vector3 startPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myPoints = 0;
+        updateText();
+        startPos = this.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalMovement = Input.GetAxis("Horizontal") * Time.deltaTime;
+        float verticalMovement = Input.GetAxis("Vertical") * Time.deltaTime;
+
+        transform.Translate(speed * horizontalMovement, 0, speed * verticalMovement, Space.Self);
+    }
+
+    public static void changePoints(int points)
+    {
+        myPoints += points;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Barn")
+        {
+            this.transform.position = startPos;
+            myPoints += 75;
+        }
+        else if (other.tag == "Respawn") {
+            this.transform.position = startPos;
+            myPoints -= 10;
+        }
+        updateText();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        updateText();
+    }
+
+    private void updateText() {
+        textPoints.text = "Points: " + myPoints;
+    }
+}
